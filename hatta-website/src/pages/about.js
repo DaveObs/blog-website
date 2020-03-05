3cb375d3619ee7a53fea3dc47e967a2e5169b5ec
DaveObs <davvid.galka@gmail.com>
2020-03-05 12:47:32 +0100

    about and gallery change

--- hatta-website/src/pages/about.js
+++ hatta-website/src/pages/about.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 
 const AboutPage = () => (
   <>
-    <h1>Hi from the second page ens penis penis</h1>
+    <h1>Hi from the second page ens</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </>
--- hatta-website/src/pages/gallery.js
+++ hatta-website/src/pages/gallery.js
@@ -7,7 +7,7 @@ import { Link } from "gatsby"
 const GalleryPage = () => (
   <>
 
-    <h1>Hi from the second page ens penis penis</h1>
+    <h1>Hi from the second page ens</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </>
